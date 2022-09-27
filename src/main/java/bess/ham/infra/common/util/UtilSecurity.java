c67c67ee098fe14348135d9e8295f17314532638
lasldjf123@naver.com <lasldjf123@naver.com>
2022-09-27 17:40:33 +0900

    1

--- src/main/java/bess/ham/infra/common/util/UtilSecurity.java
+++ src/main/java/bess/ham/infra/common/util/UtilSecurity.java
@@ -4,7 +4,12 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class UtilSecurity {
-		
+	
+	/*
+	 * 1.암호화(단방향) ex)pw 암호화전 -> 암호화후(o) 암호화후 -> 암호화전(x)
+	 * 
+	 * 2.복호화(양방향) ex)주민번호 암호화전 -> 암호화후(o) 암호화후 -> 암호화전(o)
+	 */		
     public static String encryptSha256(String text) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(text.getBytes());
